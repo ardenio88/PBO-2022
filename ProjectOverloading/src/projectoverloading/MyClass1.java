876a89c95b7fc00ba3fcb787ee5624d9036afbaf
Muhammad Arden Prabaswara <89760996+ardenio88@users.noreply.github.com>
2022-09-24 13:29:34 +0700

    Add files via upload
    LKPD 05

---
+++ ProjectOverloading/src/projectoverloading/MyClass1.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package projectoverloading;
+
+
+public class MyClass1 {
+    void myMethod(){
+        System.out.println("Hello");
+    }
+    
+    void myMethod(int x){
+        System.out.println("Holla");
+    }
+    
+    void myMethod(int x, int y){
+        System.out.println("Hollo");
+    }
+    
+    void myMethod(double x){
+        System.out.println("Hilli");
+    }
+    
+    void myMethod(int x, double y){
+        System.out.println("Hullo");
+    }
+    
+    int myMethod1(){
+        return 777;
+    }
+    
+    void myMethod(double x, double y){
+        System.out.println("Holli");
+    }
+}
