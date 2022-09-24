f0b5a95e45e4b772c5415c923bc854c48b3ff9b2
Muhammad Arden Prabaswara <89760996+ardenio88@users.noreply.github.com>
2022-09-24 13:20:11 +0700

    Add files via upload
    LKPD PBO 01

---
+++ MyProject2/src/myproject2/MyProject2.java
@@ -0,0 +1,15 @@
+package myproject2;
+
+public class MyProject2 {
+
+    public static void main(String[] args) {
+        System.out.println("   AAA      RRRRRR     DDDDDD");
+        System.out.println(" AA    AA   RR    RR   DDDDDDDD");
+        System.out.println("AA      AA  RR   RR    DDD    DD");
+        System.out.println("AAAAAAAAAA  RRRRR      DDD    DD");
+        System.out.println("AA      AA  RR  RR     DDD    DD");
+        System.out.println("AA      AA  RR   RR    DDDDDDDD");
+        System.out.println("AA      AA  RR     RR  DDDDDD");
+    }
+
+}
